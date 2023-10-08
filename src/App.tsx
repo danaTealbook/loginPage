5b74132574d256b68aae88c4650153b9fb1f018c
Dana Ken <75628245+danaTealbook@users.noreply.github.com>
2023-10-08 11:47:38 -0400

    Update App.tsx
    Removed bigger components to separate files, added login form, cutout text

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,64 @@
 import * as React from "react";
-// import Box from "@mui/material/Box";
 import "./styles.css";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import backgroundPicture from "./purpleBackground.jpeg";
+import LoginForm from "./LoginForm";
+
+function WelcomeText() {
+  return (
+    <>
+      <div className="welcome">Welcome</div>
+      <p className="welcomeContext">
+        This is a demo page for login functionality. You can enter your user
+        name and password. Since it is a demo version, it will not log you into
+        anything, but will simply trigger an alert.
+      </p>
+    </>
+  );
+}
 
 export default function App() {
   return (
     <Grid
-      display="flex"
+      container
       justifyContent="center"
+      display="flex"
       alignItems="center"
+      direction="row"
       minHeight="100vh"
     >
-      <Grid container justifyContent="center" direction="row">
-        <Grid
-          item
-          xs={12}
-          sm={6}
+      <Grid
+        item
+        xs={12}
+        md={6}
+        style={{
+          backgroundImage: `url(${backgroundPicture})`,
+          backgroundSize: "cover",
+          height: "70vh",
+          color: "#f5f5f5"
+        }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+        container
+      >
+        <WelcomeText />
+      </Grid>
+      <Grid item container xs={12} md={4}>
+        <Paper
+          elevation={0}
           style={{
-            backgroundImage: `url(${backgroundPicture})`,
-            backgroundSize: "cover",
             height: "70vh",
-            color: "#f5f5f5"
+            width: "100%",
+            justifyContent: "center",
+            display: "flex",
+            alignItems: "center"
           }}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          direction="column"
         >
-          <>
-            <p className="welcome">Welcome to the website</p>
-            <p className="welcomeContext">
-              This is a demo page for login functionality. You can enter your
-              user name and password. Since it is a demo version, it will not
-              log you into anything, but will simply trigger an alert.
-            </p>
-          </>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Grid container>
-            <Paper
-              style={{
-                height: "70vh",
-                width: "100%",
-                margin: 0
-              }}
-            >
-              <h1>Hi there</h1>
-            </Paper>
-          </Grid>
-        </Grid>
+          <LoginForm />
+        </Paper>
       </Grid>
     </Grid>
   );
